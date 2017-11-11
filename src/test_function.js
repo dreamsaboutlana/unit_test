04195fce38160169bc383293201239ce3828a27d
Sveta Golovko <dreamsaboutlana@gmail.com>
2017-11-11 14:54:35 +0200

    Add function for home testing

---
+++ src/test_function.js
@@ -0,0 +1,24 @@
+const users = [{age: 15}, {age: 14}, {age: 28}, {age: 18}, {age: 45}, {age: 68}, {age: 38}, {age: 22}, {age: 14}];
+
+const getDay = () => {
+  return new Date().getDay();
+};
+
+const getAdultUsers = (users = []) => users.filter(user => user.age > 18);
+
+const getRandomUsers = (users) => {
+  const numb = Math.random();
+
+  if (!users) {
+    return false;
+  }
+
+  const length = users.length;
+  const middleUser = Math.round(length / 2);
+
+  if (numb > 0.5) {
+    return users.slice(0, middleUser);
+  }
+
+  return users.slice(middleUser, length);
+};
